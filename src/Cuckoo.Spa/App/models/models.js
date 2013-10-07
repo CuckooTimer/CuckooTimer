157a2c16a2bf4e442009eb28d98a661c16121e1e
Aaron jackson <aaron.jackson@gmail.com>
2013-10-07 08:39:45 -0600

    Added base timer model.

---
+++ src/Cuckoo.Spa/App/models/models.js
@@ -0,0 +1,36 @@
+﻿define(function (require) {
+    function pad(num, size) {
+        var s = num + "";
+        while (s.length < size) s = "0" + s;
+        return s;
+    }
+
+    var TimerModel = function () {
+        var self = this;
+        self.totalTime = ko.observable(0);
+
+        self.totalDays = ko.computed(function () {
+            return Math.floor(self.totalTime() / 86400);
+        });
+        self.totalHours = ko.computed(function () {
+            return Math.floor((self.totalTime() - (self.totalDays() * 86400)) / 3600);
+        });
+        self.totalMinutes = ko.computed(function () {
+            return Math.floor((self.totalTime() - (self.totalDays() * 86400) - (self.totalHours() * 3600)) / 60);
+        });
+        self.totalSeconds = ko.computed(function () {
+            return pad(Math.floor((self.totalTime() - (self.totalDays() * 86400) - (self.totalHours() * 3600) - (self.totalMinutes() * 60))), 2);
+        });
+    }
+
+    var TaskModel = function (data) {
+        var self = this;
+        ko.utils.extend(self, new TimerModel());
+        ko.mapping.fromJS(data, {}, self);
+    }
+
+    return {
+        TimerModel: TimerModel,
+        TaskModel: TaskModel
+    };
+});
--- src/Cuckoo.Spa/App/services/datacontext.js
+++ src/Cuckoo.Spa/App/services/datacontext.js
@@ -1,4 +1,4 @@
-﻿define(['services/dataservice'], function (dataservice) {
+﻿define(['services/dataservice','models/models'], function (dataservice, models) {
     "use strict";
 
     var taskMapping = {
@@ -6,30 +6,9 @@
             return ko.utils.unwrapObservable(item.Id);
         },
         create: function (options) {
-            return new taskModel(options.data);
+            return new models.TaskModel(options.data);
         }
     };
-
-    var taskModel = function (data) {
-        var self = this;
-        this.totalTime = ko.observable(0);
-
-        ko.mapping.fromJS(data, {}, this);
-
-        this.totalDays = ko.computed(function () {
-            return Math.floor(self.totalTime() / 86400);
-        });
-        this.totalHours = ko.computed(function () {
-            return Math.floor((self.totalTime() - (self.totalDays() * 86400)) / 3600);
-        });
-        this.totalMinutes = ko.computed(function () {
-            return Math.floor((self.totalTime() - (self.totalDays() * 86400) - (self.totalHours() * 3600)) / 60);
-        });
-        this.totalSeconds = ko.computed(function () {
-            return pad(Math.floor((self.totalTime() - (self.totalDays() * 86400) - (self.totalHours() * 3600) - (self.totalMinutes() * 60))), 2);
-        });
-
-    }
 
     var getTasks = function (results) {
         return dataservice.getTasks()
@@ -40,11 +19,5 @@
 
     return {
         getTasks: getTasks
-    }
-
-    function pad(num, size) {
-        var s = num + "";
-        while (s.length < size) s = "0" + s;
-        return s;
     }
 });
